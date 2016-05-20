4a6a7ee0526e45f854066355c42523443b9af5d3
fejnomson <jmonson708@gmail.com>
2016-05-20 10:22:59 -0500

    Update recommendations.py

--- Chapter-2/Scripts/recommendations.py
+++ Chapter-2/Scripts/recommendations.py
@@ -90,6 +90,19 @@ def sim_pearson(prefs,p1,p2):
   return(r)
 
 
+# Returns the best matches for person from the prefs dictionary.
+# Number of results and similarity function are optional params.
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+  scores=[
+    (similarity(prefs,person,other),other)
+    for other in prefs if other!=person
+  ]
+  
+  # Sort the list so the highest scores appear at the top
+  scores.sort()
+  scores.reverse()
+  return(scores[0:n])
+
 
 
 
